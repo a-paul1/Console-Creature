fdc89bd3eec416e6c7acb039f99439b0477ac494
a-paul1 <38186956+a-paul1@users.noreply.github.com>
2022-06-07 01:39:05 -0400

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // The purpose of this program is to make an ASCII art
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("");
+      Console.WriteLine("- Casper");
+
+     }
+  }
+}
